34774f5360c273e6a80d08ee0f3205fd4668942a
David Mendoza <dmendoza99@unitec.ede>
2020-10-01 07:15:02 -0600

    requested changes of helper functions, plus i made the code smaller and simpler

--- Scripts/Miscellaneous/json2yaml/json2yaml.py
+++ Scripts/Miscellaneous/json2yaml/json2yaml.py
@@ -2,31 +2,30 @@ from json import load
 from yaml import dump
 from sys import argv
 
-jsonPath = ""
-yamlPath = ""
-
 # validated the ammout of args passed from the command line, so I could get the path from both files before starting the script
-
-if len(argv) ==1:
-	jsonPath = input("please enter the path to your JSON file: ")
-	yamlPath = input("please enter the path to your YAML file: ")
-elif len(argv) == 2:
-    jsonPath = argv[1]
-    yamlPath = input("please enter the path to your YAML file: ")
-elif len(argv) >= 3:
-	jsonPath = argv[1]
-	yamlPath = argv[2]
+def getInputAndOutputFiles():
+	if len(argv) ==1:
+		jsonPath = input("please enter the path to your JSON file: ")
+		yamlPath = input("please enter the path to your YAML file: ")
+	elif len(argv) == 2:
+		jsonPath = argv[1]
+		yamlPath = input("please enter the path to your YAML file: ")
+	elif len(argv) >= 3:
+		jsonPath = argv[1]
+		yamlPath = argv[2]
+	return jsonPath,yamlPath
 
 print("started to convert your file...")
+jsonPath,yamlPath = getInputAndOutputFiles()
 
-# from line 27 to 29
+# from line 24 to 26
 # I opened the json file 
 # after that I load the json string into a real python dict
-# after that I closed the json file
+def getJSONValueFromFile(jsonPath):
+	with open(jsonPath, "r") as jsonFile:
+		return load(jsonFile)
 
-jsonFile = open(jsonPath, "r")
-jsonValue = load(jsonFile)
-jsonFile.close()
+jsonValue = getJSONValueFromFile(jsonPath)
 
 # from line 36 to 38 
 # I opened (and created if not done) the yaml destination file
