5843e13369690955bce8c478a33e5da4da851eed
David Mendoza <dmendoza99@unitec.ede>
2020-10-01 07:24:52 -0600

    added some helper functions that where asked on other PR I made

--- Scripts/Miscellaneous/xml2csv/xml2csv.py
+++ Scripts/Miscellaneous/xml2csv/xml2csv.py
@@ -3,41 +3,44 @@ from csv import writer as CSVWriter
 from json import loads, dumps
 from sys import argv
 
-xmlPath = ""
-csvPath = ""
 
 # validated the ammout of args passed from the command line, so I could get the path from both files before starting the script
+def getInputAndOutputFiles():
+    if len(argv) == 1:
+        xmlPath = input("please enter the path to your JSON file: ")
+        csvPath = input("please enter the path to your YAML file: ")
+    elif len(argv) == 2:
+        xmlPath = argv[1]
+        csvPath = input("please enter the path to your YAML file: ")
+    elif len(argv) >= 3:
+        xmlPath = argv[1]
+        csvPath = argv[2]
+    return xmlPath, csvPath
 
-if len(argv) == 1:
-    xmlPath = input("please enter the path to your XML file: ")
-    csvPath = input("please enter the path to your CSV file: ")
-elif len(argv) == 2:
-    xmlPath = argv[1]
-    csvPath = input("please enter the path to your CSV file: ")
-elif len(argv) >= 3:
-    xmlPath = argv[1]
-    csvPath = argv[2]
+
+xmlPath, csvPath = getInputAndOutputFiles()
 
 print("started to convert your file...")
 
-# from line 29 to 33
+# from line 31 to 35
 # I read the file from the xmlPath variable, and got the dictory from parseXML
 # After that I got the first key from the dict (asumming the user passed a single xml liked the test.xml file)
 # After that I got the list of nodes inside the "root"
 # And finally I close the file
+def getChildNodesFromRoot():
+    with open(xmlPath, "r") as xmlFile:
+        data_dict = parseXML(xmlFile.read())
+        data_root = data_dict[list(data_dict.keys())[0]]
+        itemsList = list(data_root.values())[0]
+        return itemsList
 
-xmlFile = open(xmlPath, "r")
-data_dict = parseXML(xmlFile.read())
-data_root = data_dict[list(data_dict.keys())[0]]
-itemsList = list(data_root.values())[0]
-xmlFile.close()
-
+itemsList = getChildNodesFromRoot()
 # Declared a csvWrite (object to write rows on a csv file)
 csvFile = open(csvPath, "w")
 csvWriter = CSVWriter(csvFile)
 
 
-# from line 46 to 53
+# from line 49 to 57
 # I iterated over all the items on the child of the file root
 # After that i made the variable item into a real dict so I could use
 # .keys and .values, the keys for the header row
