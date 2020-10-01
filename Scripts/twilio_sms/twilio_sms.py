dea7a3c3fdabe705cc4e73f033487f06fbaebb46
Ajay-Raj-S <ajay2068155@gmail.com>
2020-10-01 19:45:00 +0530

    Add comments for better understanding

--- Scripts/twilio_sms/twilio_sms.py
+++ Scripts/twilio_sms/twilio_sms.py
@@ -6,29 +6,32 @@ import sys
 def send_message_phone(phone, body):
     account_sid = '<YOUR ACCOUNT_SID>'                      # Replace this with your account sid
     auth_token = '<YOUR AUTH_TOKEN>'                        # Replace this with your account Auth Token
+    # Create a client object from Twilio Rest
     client = Client(account_sid, auth_token)
     retry_count = 0
     while retry_count < 3:
         try:
+            # Create a client which internally call the Twilio API to send request
             message = client.messages.create(
                                 body='{}'.format(body),
                                 from_='<NEW PHONE NUMBER>', # Place your newly created phone number here,
                                 to='{}'.format(phone))
             break
         except TwilioRestException as e:
+            # This is the error code that most commonly people will encounter, because they will try to send SMS to unverified numbers
             if e.code == 21608:
                 print('You can\'t send messages to unverified numbers from trial account')
-                breakpoint()
                 return False
             print(e)
         except Exception as e:
+            # General exception which would have happened in the code
             print(e)
 
         retry_count += 1
 
     if retry_count == 3:
         return False
-
+    # Send True by default, all other cases are handled above
     return True
 
 
