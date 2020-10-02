5aa4e7845b84f7f6ddf99daaf51669ddb4db72f5
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2020-10-02 16:48:25 +0530

    Update config.py

--- Scripts/Bots/Twitter_bot/config.py
+++ Scripts/Bots/Twitter_bot/config.py
@@ -10,10 +10,10 @@ class MyStreamer(TwythonStreamer):
 			st.update_status(status='Nice Tweet @'+username, in_reply_to_status_id=id)
 			print("https://twitter.com/"+ username +"/status/" + str(id))
 
-api_k='LpkBnMbu7KQ85UybUdbiWbpG7'
-api_secret_k='l5CdR5l1SCm4H0Ga6PmYRTeyjNF1RlsUZHbkv3UefnkgAlockL'
-access_t='1271406017889185793-BEoLPylWQ1h0yYhfwhaV5qDsJVvY0P'
-access_secret_t='cd7Q902F40qbGlB17YB55GAuJJ4otFHFrFM0cFyo9tpi6'
+api_k=''
+api_secret_k=''
+access_t=''
+access_secret_t=''
 api=MyStreamer(api_k,api_secret_k,access_t,access_secret_t)
 st=Twython(api_k,api_secret_k,access_t,access_secret_t)
 keyword=input("Enter keyword to track: ")
