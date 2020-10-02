1f567c2f24a35955c8c5606a62c324058f508b3a
Mysterious-Owl <66870959+Mysterious-Owl@users.noreply.github.com>
2020-10-02 17:39:41 +0530

    Update Twitter_bot.py

--- Scripts/Bots/Twitter_bot/Twitter_bot.py
+++ Scripts/Bots/Twitter_bot/Twitter_bot.py
@@ -1,5 +1,6 @@
 from twython import Twython
 from twython import TwythonStreamer
+import configparser
 class MyStreamer(TwythonStreamer):
 	def on_success(self, data):
 		if 'text' in data:
@@ -7,15 +8,21 @@ class MyStreamer(TwythonStreamer):
 			username=data['user']['screen_name']
 			id=data['id']
 			st.create_favorite(id=id)
-   			#change 'nice tweet' too your desired retweet reply
+			#change 'nice tweet' too your desired retweet reply
 			st.update_status(status='Nice Tweet @'+username, in_reply_to_status_id=id)
-#enter your unique keys and tokens
-api_k='*'               
-api_secret_k='*'
-access_t='*'
-access_secret_t='*'
-api=MyStreamer(api_k,api_secret_k,access_t,access_secret_t)
-st=Twython(api_k,api_secret_k,access_t,access_secret_t)
+			print("https://twitter.com/"+ username +"/status/" + str(id))
+
+#enter your unique keys and tokens in config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key= config['keys']['api_key']
+api_secret_key=config['keys']['api_secret_key']
+access_token=config['keys']['access_token']
+access_secret_token=config['keys']['access_secret_token']
+
+api=MyStreamer(api_key,api_secret_key,access_token,access_secret_token)
+st=Twython(api_key,api_secret_key,access_token,access_secret_token)
 
 keyword=input("Enter keyword to track: ")
 api.statuses.filter(track=keyword)
