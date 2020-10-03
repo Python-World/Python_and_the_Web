81a8182878a932b71570e59fddb13c5cbc2b92e0
Ck Cheema <68045666+Cheemashyper@users.noreply.github.com>
2020-10-03 14:18:40 +0530

    Added Python Clicker

---
+++ Scripts/Miscellaneous/Python Clicker/Code/clicker.py
@@ -0,0 +1,13 @@
+#Importing PYAUTOGUI
+import pyautogui
+#Importing Time
+import time
+
+#Set a Sleep Time
+time.sleep(5)
+#Set a for Loop
+for i in range(200):
+    pyautogui.leftClick(467,517)
+
+#Enjoy Clicking
+#Coded by Cheemashyper
