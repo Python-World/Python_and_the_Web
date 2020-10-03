276df9b4d9020c44d4e7510dde9596b66ca09840
twozero88 <bigboyzbiz@gmail.com>
2020-10-03 14:03:55 +0530

    Generator test

--- Scripts/Miscellaneous/Screenshot-and-Share/main.py
+++ Scripts/Miscellaneous/Screenshot-and-Share/main.py
@@ -44,13 +44,13 @@ def execute():
 		pyperclip.copy("Upload failed.Maybe no internet conncetion")
 
 def on_press(key):
-	if any([key in COMBO for COMBO in COMBINATIONS]):
+	if any((key in COMBO for COMBO in COMBINATIONS)):
 		current.add(key)
 		if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
 			execute()
 
 def on_release(key):
-	if any([key in COMBO for COMBO in COMBINATIONS]):
+	if any((key in COMBO for COMBO in COMBINATIONS)):
 		current.remove(key)
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
