a7d15a10e1bbca1531931f9c8e212294a5a64c14
twozero88 <bigboyzbiz@gmail.com>
2020-10-03 12:35:08 +0530

    Indentation fixed

--- Scripts/Miscellaneous/Screenshot-and-Share/main.py
+++ Scripts/Miscellaneous/Screenshot-and-Share/main.py
@@ -44,17 +44,17 @@ def execute():
 		pyperclip.copy("Upload failed.Maybe no internet conncetion")
 
 def on_press(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
+	if any([key in COMBO for COMBO in COMBINATIONS]):
+		current.add(key)
+		if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+			execute()
 
 def on_release(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.remove(key)
+	if any([key in COMBO for COMBO in COMBINATIONS]):
+		current.remove(key)
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+	listener.join()
 
 
 #chaha0s
