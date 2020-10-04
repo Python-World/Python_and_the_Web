cafcece833467e556a7152b60d2ac2726643c4d3
captainroberts <benj@LAPTOP-VFHN599I.localdomain>
2020-10-04 14:44:52 +0800

    Initial commit for Twitter Unfollow Bot script and prerequisites

---
+++ Scripts/Bots/Twitter_Unfollow_Bot/Twitter_Unfollow.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import time
+import tweepy
+import sys
+
+auth = tweepy.auth.OAuthHandler(
+        consumer_key='xIroUblS5O8dIa2qjreI2fzB4',
+        consumer_secret='lo14kVvmDbtJAH52Bgdp3ML6KrOAp5WUqqxxeu7EP1tqkmVyvJ')
+auth.set_access_token(
+        '2212266949-N8yEYkAJWIpmHtVE2igWamMKr4kAg9UwE6HPEa3',
+        '1gG1PG5eSEHnsBYrVjaooc6hfUPnGVOiKXmFTABFvFTfV')
+
+# the following dictionaries etc aren't strictly needed for this
+# but useful for your own more in-depth apps.
+
+api=tweepy.API(auth_handler=auth, wait_on_rate_limit=True)
+
+print("Loading followers..")
+followers = []
+for follower in tweepy.Cursor(api.followers).items():
+    followers.append(follower)
+
+print("Found %s followers, finding friends.." % len(followers))
+friends = []
+for friend in tweepy.Cursor(api.friends).items():
+    friends.append(friend)
+
+# creating dictionaries based on id's is handy too
+
+friend_dict = {}
+for friend in friends:
+    friend_dict[friend.id] = friend
+
+follower_dict = {}
+for follower in followers:
+    follower_dict[follower.id] = follower
+
+# now we find all your "non_friends" - people who don't follow you
+# even though you follow them.
+
+non_friends = [friend for friend in friends if friend.id not in follower_dict]
+
+# double check, since this could be a rather traumatic operation.
+
+print("Unfollowing %s non-following users.." % len(non_friends))
+print("This will take approximately %s minutes." % (len(non_friends)/60.0))
+answer = input("Are you sure? [Y/n]").lower()
+if answer and answer[0] != "y":
+    sys.exit(1)
+
+for nf in non_friends:
+    print("Unfollowing " + str(nf.id).rjust(10))
+    try:
+        nf.unfollow()
+    except Exception:
+        print("  .. failed, sleeping for 5 seconds and then trying again.")
+        time.sleep(5)
+        nf.unfollow()
+    print(" .. completed, sleeping for 1 second.")
+    time.sleep(1)
