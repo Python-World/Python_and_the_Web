147d28af2bc38a6990c6a839521d7cd5bac63531
Aditya Jetely <ajetely@gmail.com>
2020-10-04 18:19:38 +0530

    Update Spam.py

--- Scripts/Miscellaneous/Spam/Spam.py
+++ Scripts/Miscellaneous/Spam/Spam.py
@@ -1,8 +1,13 @@
 import pyautogui
 import time
 
-time.sleep(5)
-file = open("Text.txt", "r")
-for word in file:
-    pyautogui.typewrite(word)
-    pyautogui.press("Enter")
+def main():
+    time.sleep(5)
+    file = open("Text.txt", "r")
+    for word in file:
+        pyautogui.typewrite(word)
+        pyautogui.press("Enter")
+
+        
+if __name__ == "__main__":
+    main()
