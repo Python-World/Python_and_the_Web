dbf929b16f9e1b3848f5152471917f7ba1cd513e
Aditya Jetely <ajetely@gmail.com>
2020-10-04 17:45:30 +0530

    Update MadLIbs.py

--- Scripts/Miscellaneous/Mad_Libs_Generator/MadLIbs.py
+++ Scripts/Miscellaneous/Mad_Libs_Generator/MadLIbs.py
@@ -1,18 +1,17 @@
-#python3
-##taking inputs rom users
-adjective_for_friend = input("Enter an adjective describes your friends ")
-noun_game = input("enter your favorite game's name ")
-city = input("enter your favorite city in ur country ")
-team = input("enter your favorite team's(for the game entered before) ")
-sports_shop_name = input("enter any sports shop name ")
-sports_stuff = input("enter sports accesorries(like shirts or caps or horns for cheering in game) ")
-food = input("enter ur favorite food ")
-drink = input("enter ue favorite drink ")
-##END OF INPUTS
-print("One day my "+adjective_for_friend+" friend and I decided to go to the "+noun_game+" game in "+city+". "+\
+# taking inputs rom users
+adjective_for_friend = input("Enter an adjective describes your friends: ")
+noun_game = input("enter your favorite game's name: ")
+city = input("enter your favorite city in ur country: ")
+team = input("enter your favorite team's(for the game entered before): ")
+sports_shop_name = input("enter any sports shop name: ")
+sports_stuff = input("enter sports accesorries(like shirts or caps or horns for cheering in game): ")
+food = input("enter ur favorite food: ")
+drink = input("enter ur favorite drink: ")
+# END OF INPUTS
 
-    "We really wanted to see the "+team+" play the "+noun_game+". So, we were on  our way "+
-
-    "down to the "+sports_shop_name+" and bought some "+sports_stuff+". We got into the game and "+
-    "it was a lot of fun. We ate some "+food+" and drank some "+drink+". "+\
-    "We had a great time! We plan to go again next year!" )
+print("Mad libs --------------\n")
+print(f"One day my {adjective_for_friend} friend and I decided to go to the {noun_game} game in {city}.")
+print(f"We really wanted to see the {team} play the {noun_game}.")
+print(f"So, we were on  our way down to the {sports_shop_name} and bought some {sports_stuff}.")
+print(f"We got into the game and it was a lot of fun. We ate some {food} and drank some {drink}.")
+print("We had a great time! We plan to go again next year!")
