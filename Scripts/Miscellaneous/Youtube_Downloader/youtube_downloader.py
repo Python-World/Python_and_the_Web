035af6a28dda10ed984fd797922dbf0b43a792bd
swaroopmaddu <madduswaroop@gmail.com>
2020-10-05 17:41:41 +0530

    Added Files

---
+++ Scripts/Miscellaneous/Youtube_Downloader/youtube_downloader.py
@@ -0,0 +1,24 @@
+import youtube_dl
+import sys 
+
+opts = {'outtmpl':'./videos/%(title)s.%(ext)s',}
+
+def banner():
+    print('\t"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""')
+    print("\t                            Youtube Downloader                         ")
+    print('\t"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""')
+
+def ytdl(x):
+    with youtube_dl.YoutubeDL(opts) as service:
+        service.download([x])
+
+def main():
+    banner()
+    if(len(sys.argv)<2):
+        print("Error: Invalid arguments \n Usage: python youtube_downloader url_of_video")
+    else:
+        ytdl(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
