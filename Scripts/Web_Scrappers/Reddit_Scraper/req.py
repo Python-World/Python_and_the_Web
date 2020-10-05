7e271e1d33235f8a18e7def6f094e17ba683f00f
Daniel Jo <sungkeun.jo@gmail.com>
2020-10-05 09:32:11 -0700

    Edit README, created requirements.txt, added pictures.

---
+++ Scripts/Web_Scrappers/Reddit_Scraper/req.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import praw
+
+print(pd.__version__)
--- Scripts/Web_Scrappers/Reddit_Scraper/scraper.py
+++ Scripts/Web_Scrappers/Reddit_Scraper/scraper.py
@@ -4,19 +4,24 @@ import praw
 import pandas as pd
 import sys
 
+# Too many arguments
 if len(sys.argv) > 3:
 	print('Too many arguments. EXAMPLE: % python3 scraper.py {NAME OF SUBREDDIT} {WAY TO SORT}')
+
 elif len(sys.argv) > 2:
+
+	# Create reddit object
 	reddit = praw.Reddit(client_id=config.client_id, \
 						client_secret=config.client_secret, \
 						user_agent=config.user_agent, \
 						username=config.username, \
 						password=config.password)
 
-
+	# Create subreddit object
 	sub = reddit.subreddit(sys.argv[1])
 	posts = []
 
+	# Ways to sort
 	if sys.argv[2] == 'hot':
 		posts = sub.hot(limit=10)
 	elif sys.argv[2] == 'new':
@@ -30,23 +35,30 @@ elif len(sys.argv) > 2:
 	else:
 		sys.exit('This way to sort does not exist! Please input hot, new, top, controversial, or rising.')
 
+	# Dictionary of results
 	results = {'Title':[], \
 			'URL':[], \
 			'Score':[]
 			}
 
+	# Append to dictionary
 	for result in posts:
 		results['Title'].append(result.title)
 		results['URL'].append(result.url)
 		results['Score'].append(result.score)
 
+	# Create data frame
 	results_data = pd.DataFrame(results)
 
+	# Export results to a csv file
 	results_data.to_csv(f'{sub}_{sys.argv[2]}_results.csv', index=True)
 
 	print(results_data)
-		
+
+# No way to sort
 elif len(sys.argv) > 1:
 	print('Input a way to sort!')
+
+# No subreddit
 elif len(sys.argv) > 0:
 	print('Input a subreddit!')
