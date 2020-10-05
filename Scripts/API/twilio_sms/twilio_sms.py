f70a9670636aedf67857fd0cd20ba9cfc51b7c54
Arthur <arthur93@gmail.com>
2020-10-05 15:12:40 -0300

    Fix identation spaces and optimizing imports

--- Scripts/API/twilio_sms/twilio_sms.py
+++ Scripts/API/twilio_sms/twilio_sms.py
@@ -1,11 +1,12 @@
-from twilio.rest import Client
-from twilio.base.exceptions import TwilioRestException
 import sys
+
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
 
 
 def send_message_phone(phone, body):
-    account_sid = '<YOUR ACCOUNT_SID>'                      # Replace this with your account sid
-    auth_token = '<YOUR AUTH_TOKEN>'                        # Replace this with your account Auth Token
+    account_sid = '<YOUR ACCOUNT_SID>'  # Replace this with your account sid
+    auth_token = '<YOUR AUTH_TOKEN>'  # Replace this with your account Auth Token
     # Create a client object from Twilio Rest
     client = Client(account_sid, auth_token)
     retry_count = 0
@@ -13,9 +14,9 @@ def send_message_phone(phone, body):
         try:
             # Create a client which internally call the Twilio API to send request
             message = client.messages.create(
-                                body='{}'.format(body),
-                                from_='<NEW PHONE NUMBER>', # Place your newly created phone number here,
-                                to='{}'.format(phone))
+                body='{}'.format(body),
+                from_='<NEW PHONE NUMBER>',  # Place your newly created phone number here,
+                to='{}'.format(phone))
             break
         except TwilioRestException as e:
             # This is the error code that most commonly people will encounter, because they will try to send SMS to unverified numbers
@@ -40,26 +41,26 @@ def initiate():
     message = None
 
     if len(sys.argv) > 1:
-    	if sys.argv[1] == 'help':
-    		print('\nTwilio SMS - usage')
-    		print('twilio_sms +917894561230 "Your message" - Requires phone number with country code and your SMS message')
-    		print('twilio_sms help - Prints this message\n')
-    		sys.exit()
-    	if len(sys.argv) != 3:
-    		print('twilio_sms help - for usage')
-    		sys.exit()
+        if sys.argv[1] == 'help':
+            print('\nTwilio SMS - usage')
+            print(
+                'twilio_sms +917894561230 "Your message" - Requires phone number with country code and your SMS message')
+            print('twilio_sms help - Prints this message\n')
+            sys.exit()
+        if len(sys.argv) != 3:
+            print('twilio_sms help - for usage')
+            sys.exit()
 
-    	to_phone_number = sys.argv[1]
-    	message = sys.argv[2]
-
+        to_phone_number = sys.argv[1]
+        message = sys.argv[2]
 
     if to_phone_number and message:
-    	if send_message_phone(to_phone_number, message):
-    		print('Message sent')
-    	else:
-    		print('Can\'t send message')
+        if send_message_phone(to_phone_number, message):
+            print('Message sent')
+        else:
+            print('Can\'t send message')
     else:
-    	print('Requires phone number and message, type help for proper usage')
+        print('Requires phone number and message, type help for proper usage')
 
 
 if __name__ == '__main__':
