637168f44bbd0de23811aad309282da52733f6c6
Daniel Jo <sungkeun.jo@gmail.com>
2020-10-04 17:58:12 -0700

    Reddit Scraper

---
+++ Scripts/Web_Scrappers/Reddit_Scraper/README_reddit_scraper.md
@@ -0,0 +1,13 @@
+# Reddit Scraper
+This script scrapes subreddits based on how you sort them.
+
+### Prerequisites
+Install the requirements like the following line:
+pip3 install pandas praw
+
+### How to run the script
+Run the following command with the subreddit and way to sort
+python3 scraper.py {name of subreddit} {way to sort (hot, new, top, controversial, rising}
+
+## Author Name
+[Daniel Jo](https://github.com/danieljo09)
---
+++ Scripts/Web_Scrappers/Reddit_Scraper/scraper.py
@@ -0,0 +1,52 @@
+import config
+
+import praw
+import pandas as pd
+import sys
+
+if len(sys.argv) > 3:
+	print('Too many arguments. EXAMPLE: % python3 scraper.py {NAME OF SUBREDDIT} {WAY TO SORT}')
+elif len(sys.argv) > 2:
+	reddit = praw.Reddit(client_id=config.client_id, \
+						client_secret=config.client_secret, \
+						user_agent=config.user_agent, \
+						username=config.username, \
+						password=config.password)
+
+
+	sub = reddit.subreddit(sys.argv[1])
+	posts = []
+
+	if sys.argv[2] == 'hot':
+		posts = sub.hot(limit=10)
+	elif sys.argv[2] == 'new':
+		posts = sub.new(limit=10)
+	elif sys.argv[2] == 'top':
+		posts = sub.top(limit=10)
+	elif sys.argv[2] == 'controversial':
+		posts = sub.controversial(limit=10)
+	elif sys.argv[2] == 'rising':
+		posts = sub.rising(limit=10)
+	else:
+		sys.exit('This way to sort does not exist! Please input hot, new, top, controversial, or rising.')
+
+	results = {'Title':[], \
+			'URL':[], \
+			'Score':[]
+			}
+
+	for result in posts:
+		results['Title'].append(result.title)
+		results['URL'].append(result.url)
+		results['Score'].append(result.score)
+
+	results_data = pd.DataFrame(results)
+
+	results_data.to_csv(f'{sub}_{sys.argv[2]}_results.csv', index=True)
+
+	print(results_data)
+		
+elif len(sys.argv) > 1:
+	print('Input a way to sort!')
+elif len(sys.argv) > 0:
+	print('Input a subreddit!')
