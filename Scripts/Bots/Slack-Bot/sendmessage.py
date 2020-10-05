8a5984fecccc9e102cf84cb64886095677797c0f
Yahya Fadhluloh Al Fatih <yahya.kimochi@gmail.com>
2020-10-06 00:46:08 +0700

    add slack bot and readme for tutorial

---
+++ Scripts/Bots/Slack-Bot/sendmessage.py
@@ -0,0 +1,27 @@
+from slack import WebClient
+import os
+
+# Create a slack client
+slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+
+def send_message(channel, text):
+    # message payload
+    message = {
+        "channel": channel,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": (text),
+                },
+            },
+        ],
+    }
+
+    # Post the onboarding message in Slack
+    slack_web_client.chat_postMessage(**message)
+
+
+send_message("#bebas", "Hola!")
