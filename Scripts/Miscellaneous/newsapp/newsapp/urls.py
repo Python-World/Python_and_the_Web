582fc8908302fe6e8b66b3badfe6872ea50f0399
Khushi Gangopadhyay <khushiganguly08@gmail.com>
2020-10-09 17:24:01 +0530

    Update urls.py

--- Scripts/Miscellaneous/newsapp/newsapp/urls.py
+++ Scripts/Miscellaneous/newsapp/newsapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from news import views as news_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
