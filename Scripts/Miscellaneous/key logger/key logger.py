09724c3e580f9bfd303eef6e12896629103f2df5
Aditya Jetely <ajetely@gmail.com>
2020-10-12 08:42:22 +0530

    Update key logger.py

--- Scripts/Miscellaneous/key logger/key logger.py
+++ Scripts/Miscellaneous/key logger/key logger.py
@@ -4,6 +4,11 @@ import pynput.keyboard
 def key_press(key):
 	print(key)
 
-keyboard_listner = pynput.keyboard.Listener(on_press=key_press)
-with keyboard_listner:
-	keyboard_listner.join()
+def main():
+    keyboard_listner = pynput.keyboard.Listener(on_press=key_press)
+    with keyboard_listner:
+        keyboard_listner.join()
+
+
+if __name__ == "__main__":
+    main()
