58693a75e14e1afecd16210f57309e61bda9ec6b
Ankur <ankurpan96@gmail.com>
2020-10-14 00:06:11 +0530

    dropped unused imports

--- Scripts/API/Random_Album_API/Random_Album_API/views/api_view.py
+++ Scripts/API/Random_Album_API/Random_Album_API/views/api_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, jsonify, request, abort
+from flask import Blueprint, jsonify, request
 from flask.views import MethodView
 
 from Random_Album_API.shared_resources.argument_check import validate_arguments
