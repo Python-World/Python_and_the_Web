6a6e314dff397d4a41609cd61c129ed0b5f274e8
Reean Khan <reean.khan@student.manchester.ac.uk>
2020-10-18 20:27:38 +0100

    small changes

--- Scripts/API/Fork and Clone/Fnc.py
+++ Scripts/API/Fork and Clone/Fnc.py
@@ -1,24 +1,28 @@
 import subprocess
+import msvcrt
+import sys
 
-def fork(username, password, repo, owner): 
+
+def fork(): 
+    username = input("Your github username: ")
+    password = input("Your github password: ")
+    repo = input("Name of repo you want to fork: ")
+    owner = input("Username of repo owner: ")
     subprocess.run("curl -X POST -u " + username + ":" + password +  " https://api.github.com/repos/" + owner + "/" + repo + "/forks" , shell=True)
+    answer = input("Did it fork properly (y/n)? ")
+    if answer == "n": fork() 
+    clone(username, repo)
 
 def clone(username, repo):
 
     location = input("Path for cloning repo into (if current current directory press enter): ")
     if len(location):
         subprocess.run("git clone https://github.com/"+ username + "/" + repo + ".git " + location , shell=True)
-        answer = input("Did it clone properly (y/n?")
+        answer = input("Did it clone properly (y/n)?")
         if answer == "n": clone(username, repo) 
     else:
         subprocess.run("git clone https://github.com/"+ username + "/" + repo + ".git" , shell=True)
 
 if __name__ == "__main__":
-    username = input("Your github username: ")
-    password = input("Your github password: ")
-    owner = input("Name of repo you want to fork: ")
-    repo = input("Username of repo owner: ")
-    
-    fork(username, password, repo, owner)
-    clone(username, repo)
+    fork()
 
---
+++ Scripts/API/Fork and Clone/README.md
@@ -0,0 +1,20 @@
+# A Command Line Tool that Uses the Github API to Form and Clone Repos
+<!--Remove the below lines and add yours -->
+Using this convertor the existing value of currencies can be converted into the value of CryptoCurrencies.
+
+### Prerequisites
+<!--Remove the below lines and add yours -->
+-> You need to have github installed on your system so git clone can run from the script.
+
+-> Install the package using pip install -r requirements.txt.
+### How to run the script
+<!--Remove the below lines and add yours -->
+python Fnc.py
+
+### Screenshot/GIF showing the sample use of the script
+<!--Remove the below lines and add yours -->
+Screenshot showing sample use of script is present in example.png
+
+## *Author Name*
+<!--Remove the below lines and add yours -->
+Reean Khan
