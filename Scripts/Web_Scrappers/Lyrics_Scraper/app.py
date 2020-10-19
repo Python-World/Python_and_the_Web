cdc73f2d714617e07d9efb90bbc2910603a21c7c
ankdos <ankurpan96@gmail.com>
2020-10-19 19:11:42 +0530

    Scrape lyrics done

--- Scripts/Web_Scrappers/Lyrics_Scraper/app.py
+++ Scripts/Web_Scrappers/Lyrics_Scraper/app.py
@@ -10,16 +10,40 @@ class ScrapeLyrics:
         """"""
         self.url = url
         self.type = parse_type
+        self.soup = None
         self.out = None
         
     def parse(self):
         """"""
-    
+        res = requests.get(self.url)
+        self.soup = BeautifulSoup(res.text,'html.parser')
+        if self.parse_type == 'song_list':
+            self.parse_songlist()
+        else:
+            self.parse_lyrics()
+            
     def parse_songlist(self):
         """"""
+        self.out = []
+        for tds in self.soup.find_all('td'):
+            data = tds.attrs.get('onclick',None)
+            if data :
+                self.out.append((tds.text, data.split('=')[-1]))
     
     def parse_lyrics(self):
         """"""
+        for tds in soup.find_all('div'):
+            if not tds.attrs:
+                self.out = tds.text
+    
+    def driver(self):
+        """"""
+        self.parse()
+        if self.parse_type == 'song_list':
+            self.parse_songlist()
+        else:
+            self.parse_lyrics()
+        return self.out
 
 class LyricsConsole(ScrapeLyrics):
     """Class for search and display"""
