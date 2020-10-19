67d50ebd9d3c4832014369b77e67cb59966a572b
ankdos <ankurpan96@gmail.com>
2020-10-19 19:02:17 +0530

    blueprint ready

---
+++ Scripts/Web_Scrappers/Lyrics_Scraper/app.py
@@ -0,0 +1,35 @@
+import requests
+
+from bs4 import BeautifulSoup
+from whaaaaat import prompt, print_json, Separator
+
+class ScrapeLyrics:
+    """Class to scrape lyrics"""
+    
+    def __init__(self, url, parse_type):
+        """"""
+        self.url = url
+        self.type = parse_type
+        self.out = None
+        
+    def parse(self):
+        """"""
+    
+    def parse_songlist(self):
+        """"""
+    
+    def parse_lyrics(self):
+        """"""
+
+class LyricsConsole(ScrapeLyrics):
+    """Class for search and display"""
+    
+    def __init__(self, song_name):
+        """"""
+        self.song_name = song_name
+        
+    def search_song(self):
+        """"""
+    
+    def display_prompt():
+        """"""
