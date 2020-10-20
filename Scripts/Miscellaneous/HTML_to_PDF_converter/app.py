cb04e2b174a230295f49ac75cee0a094815863bf
ankdos <ankurpan96@gmail.com>
2020-10-20 23:03:53 +0530

    added arg parser

--- Scripts/Miscellaneous/HTML_to_PDF_converter/app.py
+++ Scripts/Miscellaneous/HTML_to_PDF_converter/app.py
@@ -1,9 +1,26 @@
+import sys
+import argparse
 import weasyprint
 
 class Html2Pdf:
     """"""
-    def __init__(self, type_, url):
+    
+    def __init__(self, url, output_filename):
         """"""
-        self.type = type_
         self.url = url
-        
+        self.output_filename = output_filename
+    
+    def get_pdf(self):
+        """"""
+        pdf = weasyprint.HTML(self.url).write_pdf()
+        file_name = 'outputs/' + self.output_filename
+        open(file_name, 'wb').write(pdf)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-inp", "--input", help="input file url")
+    parser.add_argument("-out", "--output", help="output file name")
+    args = parser.parse_args()
+    obj = Html2Pdf(url=args.input, output_filename=args.output)
+    obj.get_pdf()
